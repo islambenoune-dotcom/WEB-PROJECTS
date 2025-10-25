474b39a394e8777585c49b4c1729b48c32f33935
islambenoune-dotcom <fahimakerghel@gmail.com>
2025-10-25 11:48:09 +0100

    Add files via upload

---
+++ functions  parmeter.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int myareafunction(int num1,int num2)
+{
+
+    return num1 * num2;
+}
+int main()
+
+{
+    cout << myareafunction(10, 20) << endl;
+}
+
