a167f34d164f7d9f1d4bcfe530336e51b1a2e7f3
ISLAMBEN <fahimakerghel@gmail.com>
2025-11-01 13:55:21 +0100

    Add files via upload

---
+++ IF ELSE STATEMENT.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int X;
+	cout << "please enter a number " << endl;
+	cin >> X;
+	if (X > 5)
+	{
+		cout << "the code is correct" << endl;
+	}
+	else
+	{
+
+		cout << "the code is false" << endl;
+	}
+}
+
